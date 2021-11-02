8b45b60b91b487f45edfa5aed9eedd6502256317
H-SChan <dlwp7581@gmail.com>
2021-11-02 09:07:15 +0900

    [update] school list

--- src/main/java/com/mo/domain/dto/school/res/SchoolListRes.java
+++ src/main/java/com/mo/domain/dto/school/res/SchoolListRes.java
@@ -4,6 +4,7 @@ import com.mo.domain.dto.estimate.data.EstimateDataDto;
 import com.mo.domain.entity.School;
 import com.mo.enums.school.FondType;
 import com.mo.enums.school.GenderCheck;
+import com.mo.enums.school.SchoolKind;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,14 +15,14 @@ public class SchoolListRes {
     private Double estimate;
     private int estimatedPeople;
     private GenderCheck genderCheck;
-    private FondType fondType;
+    private SchoolKind fondType;
     private String roadNameAddress;
 
     public SchoolListRes(School school, EstimateDataDto dto) {
         this.idx = school.getIdx();
         this.schoolName = school.getName();
         this.genderCheck = school.getGenderCheck();
-        this.fondType = school.getFondType();
+        this.fondType = school.getSchoolKind();
         this.roadNameAddress = school.getRoadNameAddress();
         this.estimate = dto.getEstimateAvg();
         this.estimatedPeople = dto.getEstimated();
