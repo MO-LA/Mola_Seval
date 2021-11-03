c373bdb9e8e598c508cf682b5960a51bd9c8fde7
H-SChan <dlwp7581@gmail.com>
2021-11-03 09:21:03 +0900

    [update] token

--- src/main/java/com/mo/config/SwaggerConfig.java
+++ src/main/java/com/mo/config/SwaggerConfig.java
@@ -1,12 +1,19 @@
 package com.mo.config;
 
+import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @EnableSwagger2
 @Configuration
@@ -14,10 +21,22 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api() {
+        List<Parameter> global = new ArrayList<>();
+        global.add(
+                new ParameterBuilder().name("Authorization")
+                        .description("Bearer Token")
+                        .parameterType("header")
+                        .required(false)
+                        .modelRef(new ModelRef("string"))
+                        .build()
+        );
+
         return new Docket(DocumentationType.SWAGGER_2)
+                .globalOperationParameters(global)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.mo"))
                 .paths(PathSelectors.any())
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
                 .build();
     }
 }
